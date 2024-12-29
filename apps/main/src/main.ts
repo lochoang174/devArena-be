6fc312e1647be511506c5caad67b2324d74b5192
LTP <lamtruongphu2003@gmail.com>
2024-12-29 18:44:46 +0700

    fix cors error in main.ts

--- apps/main/src/main.ts
+++ apps/main/src/main.ts
@@ -1,10 +1,10 @@
-import { NestFactory, Reflector } from '@nestjs/core';
-import { MainModule } from './main.module';
-import { ValidationPipe } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { MAIN_SERVICE, TransformInterceptor } from '@app/common';
-import { Transport } from '@nestjs/microservices';
-import { JwtAuthGuard } from './auth/guards/jwt.guard';
+import { NestFactory, Reflector } from "@nestjs/core";
+import { MainModule } from "./main.module";
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { MAIN_SERVICE, TransformInterceptor } from "@app/common";
+import { Transport } from "@nestjs/microservices";
+import { JwtAuthGuard } from "./auth/guards/jwt.guard";
 
 async function bootstrap() {
   const app = await NestFactory.create(MainModule);
@@ -14,19 +14,23 @@ async function bootstrap() {
     name: MAIN_SERVICE,
     transport: Transport.TCP,
     options: {
-      host: '0.0.0.0',
-      port: configService.get('TCP_PORT'),
+      host: "0.0.0.0",
+      port: configService.get("TCP_PORT"),
     },
   });
-  const reflector = app.get(Reflector)
+  const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
 
-  app.enableCors();
+  app.enableCors({
+    origin: configService.get("FRONTEND_URL"), // Specify the allowed origin(s)
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed methods
+    credentials: true, // Include credentials (cookies, authorization headers, etc.)
+  });
   app.useGlobalPipes(new ValidationPipe());
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix("api");
   app.useGlobalInterceptors(new TransformInterceptor(reflector));
 
-  const port = configService.get<number>('PORT');
+  const port = configService.get<number>("PORT");
 
   await app.listen(port);
 }
