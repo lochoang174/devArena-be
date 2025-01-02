53a20b786dcbf0225a727c6e0cfc659c91a06c27
LTP <lamtruongphu2003@gmail.com>
2025-01-02 08:46:23 +0700

    redefine PredefinedTags

--- apps/main/src/schemas/exercise.schema.ts
+++ apps/main/src/schemas/exercise.schema.ts
@@ -1,39 +1,95 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document, Types } from 'mongoose';
-import { ObjectType, Field, ID, registerEnumType } from '@nestjs/graphql';
-import { Testcase, TestcaseSchema } from './testcase.schema';
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Types } from "mongoose";
+import { ObjectType, Field, ID, registerEnumType } from "@nestjs/graphql";
+import { Testcase, TestcaseSchema } from "./testcase.schema";
 
 // Register enums with GraphQL
 export enum LanguageEnum {
-  Java = 'java',
-  C = 'c',
-  Cpp = 'cpp',
+  Java = "java",
+  C = "c",
+  Cpp = "cpp",
 }
 
 export enum DifficultyEnum {
-  Easy = 'Easy',
-  Medium = 'Medium',
-  Hard = 'Hard',
+  Easy = "easy",
+  Medium = "medium",
+  Hard = "hard",
 }
 
 registerEnumType(DifficultyEnum, {
-  name: 'DifficultyEnum',
-  description: 'The difficulty levels for exercises',
+  name: "DifficultyEnum",
+  description: "The difficulty levels for exercises",
 });
 
 export const PredefinedTags = [
-  'Array',
-  'Function',
-  'Loop',
-  'String',
-  'Recursion',
-  'Algorithm',
+  "array",
+  "string",
+  "hash-table",
+  "dynamic-programming",
+  "math",
+  "sorting",
+  "greedy",
+  "depth-first-search",
+  "database",
+  "binary-search",
+  "matrix",
+  "tree",
+  "breadth-first-search",
+  "bit-manipulation",
+  "two-pointers",
+  "prefix-sum",
+  "heap-(priority-queue)",
+  "binary-tree",
+  "simulation",
+  "stack",
+  "graph",
+  "counting",
+  "sliding-window",
+  "design",
+  "backtracking",
+  "enumeration",
+  "union-find",
+  "linked-list",
+  "number-theory",
+  "ordered-set",
+  "monotonic-stack",
+  "trie",
+  "segment-tree",
+  "bitmask",
+  "queue",
+  "divide-and-conquer",
+  "recursion",
+  "combinatorics",
+  "binary-indexed-tree",
+  "geometry",
+  "binary-search-tree",
+  "hash-function",
+  "memoization",
+  "string-matching",
+  "topological-sort",
+  "shortest-path",
+  "rolling-hash",
+  "game-theory",
+  "interactive",
+  "data-stream",
+  "monotonic-queue",
+  "brainteaser",
+  "randomized",
+  "merge-sort",
+  "doubly-linked-list",
+  "counting-sort",
+  "iterator",
+  "concurrency",
+  "probability-and-statistics",
+  "quickselect",
+  "suffix-array",
+  "bucket-sort",
 ];
 
 @ObjectType() // GraphQL ObjectType for Exercise
 @Schema({
   timestamps: true, // Adds createdAt and updatedAt fields
-  discriminatorKey: 'type',
+  discriminatorKey: "type",
 })
 export class Exercise {
   @Field(() => ID) // ID type for GraphQL
@@ -58,11 +114,10 @@ export class Exercise {
     validate: {
       validator: (tags: string[]) =>
         tags.every((tag) => PredefinedTags.includes(tag)),
-      message: 'Tags must be one of the predefined values',
+      message: "Tags must be one of the predefined values",
     },
   })
   tags: string[];
-
 
   @Field(() => [Testcase]) // Array of Testcase type for GraphQL
   @Prop({
@@ -71,11 +126,10 @@ export class Exercise {
   })
   testcases: Testcase[];
 
-  //score 
+  //score
   @Field(() => Number)
   @Prop({ required: true })
   score: number;
-
 }
 
 export type ExerciseDocument = Exercise & Document;
