9d00de441501ee1164bea5ba1d4c1426ec897cab
LTP <lamtruongphu2003@gmail.com>
2025-01-17 13:23:02 +0700

    change emit complete_submit

--- apps/main/src/socket/socket.gateway.ts
+++ apps/main/src/socket/socket.gateway.ts
@@ -1,50 +1,59 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody, ConnectedSocket, WebSocketServer, OnGatewayConnection } from '@nestjs/websockets';
-import { SocketService } from './socket.service';
-import { CreateSocketDto } from './dto/create-socket.dto';
-import { UpdateSocketDto } from './dto/update-socket.dto';
-import { Server, Socket } from 'socket.io';
-import { ExerciseService } from '../exercise/exercise.service';
-import { StudyService } from '../study/study.service';
-import { COMPILE_SERVICE_NAME, CompileServiceClient, CurrentUser, IUser, TestCase } from '@app/common';
-import { Inject, OnModuleInit } from '@nestjs/common';
-import { ClientGrpc } from '@nestjs/microservices';
-import { Testcase } from '../schemas/testcase.schema';
-import { ExerciseStatusService } from '../exercise-status/exercise-status.service';
-import { CreateExerciseStatusDto } from '../exercise-status/dto/create-exercise-status.dto';
+import {
+  WebSocketGateway,
+  SubscribeMessage,
+  MessageBody,
+  ConnectedSocket,
+  WebSocketServer,
+  OnGatewayConnection,
+} from "@nestjs/websockets";
+import { SocketService } from "./socket.service";
+import { CreateSocketDto } from "./dto/create-socket.dto";
+import { UpdateSocketDto } from "./dto/update-socket.dto";
+import { Server, Socket } from "socket.io";
+import { ExerciseService } from "../exercise/exercise.service";
+import { StudyService } from "../study/study.service";
+import {
+  COMPILE_SERVICE_NAME,
+  CompileResult,
+  CompileServiceClient,
+  CompileStatus,
+  CurrentUser,
+  IUser,
+  TestCase,
+} from "@app/common";
+import { Inject, OnModuleInit } from "@nestjs/common";
+import { ClientGrpc } from "@nestjs/microservices";
+import { Testcase } from "../schemas/testcase.schema";
+import { ExerciseStatusService } from "../exercise-status/exercise-status.service";
+import { CreateExerciseStatusDto } from "../exercise-status/dto/create-exercise-status.dto";
+import e from "express";
 
 @WebSocketGateway({
   cors: {
-    origin: '*',
+    origin: "*",
   },
-  path: '/socket.io/',
-  transports: ['websocket', 'polling'],
-
-
+  path: "/socket.io/",
+  transports: ["websocket", "polling"],
 })
 export class SocketGateway implements OnGatewayConnection, OnModuleInit {
   @WebSocketServer() server: Server;
-  private compileService: CompileServiceClient
+  private compileService: CompileServiceClient;
 
   constructor(
     private readonly socketService: SocketService,
     private studyService: StudyService,
     private exerciseService: ExerciseService,
     private exerciseStatusService: ExerciseStatusService,
-    @Inject(COMPILE_SERVICE_NAME) private client: ClientGrpc
-
-
-  ) { }
+    @Inject(COMPILE_SERVICE_NAME) private client: ClientGrpc,
+  ) {}
   onModuleInit() {
     this.compileService =
       this.client.getService<CompileServiceClient>(COMPILE_SERVICE_NAME);
   }
   // This method is called when a new user connects
   handleConnection(client: Socket) {
-    console.log('A new user connected:', client.id);
-
-
+    console.log("A new user connected:", client.id);
   }
-
 
   // Hàm lấy mã giải pháp
   async getSolutionCode(exerciseId: string): Promise<string> {
@@ -56,10 +65,8 @@ export class SocketGateway implements OnGatewayConnection, OnModuleInit {
     }
   }
 
-
-
   // Register a user with a uniqueId
-  @SubscribeMessage('register')
+  @SubscribeMessage("register")
   handleRegister(
     @MessageBody() data: { uniqueId: string },
     @ConnectedSocket() client: Socket,
@@ -71,25 +78,35 @@ export class SocketGateway implements OnGatewayConnection, OnModuleInit {
     this.socketService.associateClientWithUniqueId(client.id, data.uniqueId);
 
     // (Optional) Send response back to client
-    client.emit('registered', { message: 'User registered successfully' });
+    client.emit("registered", { message: "User registered successfully" });
   }
 
   // This is an example of handling a custom message from a client
-  @SubscribeMessage('sendMessage')
-  handleMessage(@MessageBody() message: string, @ConnectedSocket() client: Socket): void {
-    console.log('Received message from client:', message);
-    this.server.emit('message', { clientId: client.id, message });
+  @SubscribeMessage("sendMessage")
+  handleMessage(
+    @MessageBody() message: string,
+    @ConnectedSocket() client: Socket,
+  ): void {
+    console.log("Received message from client:", message);
+    this.server.emit("message", { clientId: client.id, message });
   }
 
-  @SubscribeMessage('compile')
+  @SubscribeMessage("compile")
   async compile2(
-    @MessageBody() data: { code: string; testCases: string[][], exerciseId: string, language: string, userId: string },
+    @MessageBody()
+    data: {
+      code: string;
+      testCases: string[][];
+      exerciseId: string;
+      language: string;
+      userId: string;
+    },
     @ConnectedSocket() client: Socket,
   ) {
     try {
       const userId = data.userId;
       if (!userId) {
-        throw new Error('User information is missing');
+        throw new Error("User information is missing");
       }
       const compileRequest = {
         code: data.code,
@@ -98,92 +115,143 @@ export class SocketGateway implements OnGatewayConnection, OnModuleInit {
         language: data.language,
       };
 
-      if (!(await this.exerciseStatusService.checkExist(userId, data.exerciseId))) {
+      if (
+        !(await this.exerciseStatusService.checkExist(userId, data.exerciseId))
+      ) {
         const createExerciseStatusDto: CreateExerciseStatusDto = {
           userId: userId,
           exerciseId: data.exerciseId,
-          submission: []
-        }
+          submission: [],
+        };
 
-        await this.exerciseStatusService.create(createExerciseStatusDto)
+        await this.exerciseStatusService.create(createExerciseStatusDto);
       }
 
       const stream = this.compileService.runCompile(compileRequest);
 
       stream.subscribe({
         next: (status) => {
-          client.emit('output', status);
+          client.emit("output", status);
         },
         error: (err) => {
-          client.emit('error', { message: err.details });
+          client.emit("error", { message: err.details });
         },
         complete: () => {
-          client.emit('completed', `Biên dịch hoàn tất tất cả test cases cho user ${userId}`);
+          client.emit(
+            "completed",
+            `Biên dịch hoàn tất tất cả test cases cho user ${userId}`,
+          );
         },
       });
     } catch (err) {
-      client.emit('error', { message: err.message });
+      client.emit("error", { message: err.message });
     }
   }
 
-  @SubscribeMessage('submit')
+  @SubscribeMessage("submit")
   async submit(
-    @MessageBody() data: { code: string, exerciseId: string, userId: string, language: string, },
+    @MessageBody()
+    data: {
+      code: string;
+      exerciseId: string;
+      userId: string;
+      language: string;
+    },
     @ConnectedSocket() client: Socket,
-
   ) {
-    const tc = await this.exerciseService.findTestcaseById(data.exerciseId)
+    const tc = await this.exerciseService.findTestcaseById(data.exerciseId);
     const payload: TestCase[] = tc.map((ele, i) => {
-      const temp = ele.input.map((e) => {
-        // Lấy giá trị trong object (giả sử bạn có một object với giá trị cần lấy)
-        return Object.values(e).map(val => String(val)); // Chuyển đổi tất cả giá trị thành string
-      }).flat();
+      const temp = ele.input
+        .map((e) => {
+          // Lấy giá trị trong object (giả sử bạn có một object với giá trị cần lấy)
+          return Object.values(e).map((val) => String(val)); // Chuyển đổi tất cả giá trị thành string
+        })
+        .flat();
       return {
         // inputs: temp as string[][],
         // output: ele.output,
         output: ele.output,
-        inputs: temp
-      }
+        inputs: temp,
+      };
+    });
 
-    })
-    const codeSolution = await this.getSolutionCode(data.exerciseId)
+    const codeSolution = await this.getSolutionCode(data.exerciseId);
     const stream = this.compileService.runSubmit({
       testcases: payload,
       code: data.code,
       codeSolution,
-      language: data.language
-    })
-
+      language: data.language,
+    });
+    const arrayStatus: CompileStatus[] = [];
     stream.subscribe({
       next: async (res) => {
         if (res.status) {
-          client.emit('output_submit', res.status); // Truyền kết quả về client qua socket
-
+          arrayStatus.push(res.status);
         } else {
-          const { result, score, status } = res.finalResult
-          let compareTime = 0
+          const { result, score, status } = res.finalResult;
+          let compareTime = 0;
           if (status === 200) {
-
-            this.exerciseStatusService.updateStatusAndSubmission(data.userId, data.exerciseId, "completed", data.code, "accepted", result, score, res.finalResult.totalRuntime)
-            compareTime = await this.exerciseStatusService.compareRuntime(data.exerciseId, res.finalResult.totalRuntime)
+            this.exerciseStatusService.updateStatusAndSubmission(
+              data.userId,
+              data.exerciseId,
+              "completed",
+              data.code,
+              "accepted",
+              result,
+              score,
+              res.finalResult.totalRuntime,
+            );
+            compareTime = await this.exerciseStatusService.compareRuntime(
+              data.exerciseId,
+              res.finalResult.totalRuntime,
+            );
           } else {
-            this.exerciseStatusService.updateStatusAndSubmission(data.userId, data.exerciseId, "in-progress", data.code, "wrong answer", result, score, res.finalResult.totalRuntime)
-
+            this.exerciseStatusService.updateStatusAndSubmission(
+              data.userId,
+              data.exerciseId,
+              "in-progress",
+              data.code,
+              "wrong answer",
+              result,
+              score,
+              res.finalResult.totalRuntime,
+            );
           }
-          client.emit('complete_submit', {
+          client.emit("complete_submit", {
             ...res.finalResult,
-            compareTime: compareTime
+            testcases: tc
+              .filter((ele, index) =>
+                arrayStatus.some(
+                  (arrayStatus) =>
+                    arrayStatus.testCaseIndex === index &&
+                    !arrayStatus.isCorrect,
+                ),
+              )
+              .map((ele, index) => ({
+                ...ele,
+                output: arrayStatus.find(
+                  (arrayStatus) =>
+                    arrayStatus.testCaseIndex === index &&
+                    !arrayStatus.isCorrect,
+                ).output,
+                outputExpected: ele.output,
+              })),
+            arrayStatus,
+            compareTime: compareTime,
           }); // Truyền kết quả về client qua socket
-
         }
       },
       error: (error) => {
-
-        this.exerciseStatusService.updateStatusAndSubmission(data.userId, data.exerciseId, "in-progress", data.code, "compile error", `0/${tc.length}`)
-        client.emit('error', { message: error.details });
+        this.exerciseStatusService.updateStatusAndSubmission(
+          data.userId,
+          data.exerciseId,
+          "in-progress",
+          data.code,
+          "compile error",
+          `0/${tc.length}`,
+        );
+        client.emit("error", { message: error.details });
       },
-
     });
-
   }
 }
